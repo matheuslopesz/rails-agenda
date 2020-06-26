ed44b439e02b6017b807a7ca9e357805a0f6f607
matheus lopes <matheuslopes.developer@gmail.com>
2020-06-26 20:34:07 -0300

    add the not authorized test

--- spec/api/v1/controllers/messages_controller_spec.rb
+++ spec/api/v1/controllers/messages_controller_spec.rb
@@ -4,29 +4,30 @@ RSpec.describe Api::V1::MessagesController, type: :controller do
 
   let(:user) { FactoryBot.create(:user)}
   let(:user1) { FactoryBot.create(:user)}
+  let(:invalid_user) { FactoryBot.create(:invalid_user)}
   let(:message) { FactoryBot.create(:message,to: user.id,from: user1.id)}
   let(:archived_message) { FactoryBot.create(:message,:archived,to: user.id)}
 
 
   describe '#index' do
-    context 'when is normal user and valid' do
-      controller do
-        def api_user
-          FactoryBot.create(:user)
-        end
-      end
-      it 'list all personal non archived messages' do
-        subject { controller.api_user }
-        controller.instance_variable_set(:api_user)
-        get :index, params: { token:'ADMI131654' }
-        expect(assigns(:messages)).to eq([message])
+    before do
+      allow_any_instance_of(Api::V1::MessagesController).to receive(:api_user) { user }
+    end
+    context 'when is master user' do
+      it 'when master user list all not archived messages' do
+        get :index, params: { token: user.token, permission: 'master' }
+        expect(response).to have_http_status(:success)
+      end 
+
+      it 'when token default user list that were sent to user' do
+        get :index, params: { token: user.token, permission: 'user'}
+        expect(response).to have_http_status(:success)
       end
 
-      it 'can be reached' do
-        get :index
-        expect(response).to render_template :index
-      end
-
+      it 'when invalid token' do
+        get :index, params: { token: 'invalid_token'}
+        expect(response).to have_http_status(:unauthorized)
+      end         
     end
   end
 
