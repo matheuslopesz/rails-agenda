73600ce6b22ac8682cc9a12df710b56ce82e826a
matheus lopes <matheuslopes.developer@gmail.com>
2020-06-27 18:04:29 -0300

    add users index test

--- spec/api/v1/controllers/users_controller_spec.rb
+++ spec/api/v1/controllers/users_controller_spec.rb
@@ -1,20 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
+  let(:user) { FactoryBot.create(:user)}
 
   
   describe '#index' do
+    before do
+      allow_any_instance_of(Api::V1::UsersController).to receive(:api_user) { user }
+    end
+
     context 'list all users when is master user' do
-      it 'when master user list all not archived messages' do
-        
+      it 'when is master' do
+        get :index, params: { token: user.token, permission: 'master' }
+        expect(response).to have_http_status(:success)
       end 
 
       it 'when user is not master' do
-        
+        get :index, params: { token: user.token, permission: 'user' }
+        expect(response).to have_http_status(:unauthorized)
       end
 
       it 'when invalid token' do
-        
+        get :index, params: { token: 'invalid_token', permission: 'user' }
+        expect(response).to have_http_status(:unauthorized)
       end  
 
     end
